81ac005e352f9f4c7c157a3f36828af90cf931d6
yeasinali58557-hue <yeasinali58557@gmail.com>
2026-09-09 15:30:06 +0600

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def run_dummy_server():
 logging.basicConfig(level=logging.INFO)
 
 # Updated Token
-TOKEN = "8736488112:AAFp7aT_5N13ASSZK6UR9IZFWIrxz_RTt-E"
+TOKEN = "8736488112:AAHTHjThZMFND1pS0aITTImIKJaINzQL3Jk
 ADMIN_ID = 7753794493
 DB_FILE = "bot_data.db"
 REFERRAL_BONUS = 2.0  # Per referral bonus in BDT
